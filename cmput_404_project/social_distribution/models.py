7daa66170734e3dbc9ef238569ce91a80b88f40f
Jejoon Ryu <rjejoon@gmail.com>
2022-02-21 23:32:31 -0700

    Migrate Author model

--- cmput_404_project/social_distribution/models.py
+++ cmput_404_project/social_distribution/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser 
+from django.utils import timezone
+
+from .managers import AuthorManager
+
+class Author(AbstractUser):
+
+    class Meta:
+        verbose_name = 'Author'
+
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    host = models.URLField()
+    github = models.URLField()
+    profile_image = models.URLField()
+    
+    objects = AuthorManager()
+
+
+    def __str__(self):
+        return self.username
+
+
