8c095bc016da991f00793f6fbd1e74f1453ff5b2
Felipe <rdrgues@hotmail.com>
2022-02-24 15:58:02 -0700

    added option to edit own posts

--- cmput_404_project/posts/views.py
+++ cmput_404_project/posts/views.py
@@ -12,6 +12,48 @@ def display_public_posts(request):
 
     return render(request, 'posts/public_posts.html', {'posts': posts})
 
+def display_own_posts(request):
+    posts = Post.objects.filter(author=request.user)
+
+    return render(request, 'posts/own_posts.html', {'posts': posts})
+
+    # return HttpResponse("test")
+
+def edit_post(request, id):
+    post = Post.objects.filter(id=id)[0]
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        obj = form.save(commit=False)                
+        obj.author = post.author
+        obj.id = post.id
+
+        # TODO set proper URls
+        obj.source = post.source
+        obj.origin = post.origin
+
+        obj.save()
+
+        return redirect("/")
+    else:
+        # post = Post.objects.filter(id=id)[0]
+
+        data = {
+            'title': post.title,
+            'description': post.description,
+            # 'author': post.author,
+            # 'id': post.id,
+        }
+
+        form = PostForm(data)
+
+        # form['title'] = post.title
+        # form['description'] = post.description
+
+        return render(request, "posts/new_post.html", {'form': form})
+
+    return HttpResponse(id)
+
 def new_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
