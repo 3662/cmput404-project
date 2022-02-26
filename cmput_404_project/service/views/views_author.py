20a3e5107cd3b5f09fed3d9ff8eb815a8418f861
Jejoon Ryu <rjejoon@gmail.com>
2022-02-25 20:58:24 -0700

    Add doc

--- cmput_404_project/service/views/views_author.py
+++ cmput_404_project/service/views/views_author.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
 from django.core.paginator import Paginator, EmptyPage
 from django.http import Http404, JsonResponse
@@ -11,6 +11,11 @@ DEFAULT_SIZE = 25
 
 @require_http_methods(["GET"])
 def authors_detail(request):
+    '''
+    Returns JSON response of the details of the authors.
+
+    Default page = 1, size = 25
+    '''
     page = int(request.GET.get('page', DEFAULT_PAGE))
     size = int(request.GET.get('size', DEFAULT_SIZE))
 
@@ -26,7 +31,19 @@ def authors_detail(request):
     return JsonResponse(data)
 
 
+@require_http_methods(["GET"])
+def author_detail(request, author_id):
+    '''
+    Returns JSON response of the detail of the author with author_id.
+    '''
+    author = get_object_or_404(Author, pk=author_id)
+    return JsonResponse(get_author_detail(author))
+
+
 def get_author_detail(author) -> dict:
+    '''
+    Returns a dict containing the author information.
+    '''
     author_d = {}
     author_d['type'] = 'author'
     author_d['id'] = f"{author.host}authors/{author.id}"
@@ -37,9 +54,3 @@ def get_author_detail(author) -> dict:
     author_d['profileImage'] = author.profile_image
 
     return author_d
-
-
-@require_http_methods(["GET"])
-def author_detail(request, author_id):
-    author = get_object_or_404(Author, pk=author_id)
-    return JsonResponse(get_author_detail(author))
