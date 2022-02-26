5c34f33935a5b113f0bc67e23cc75ab31a554cf4
Jejoon Ryu <rjejoon@gmail.com>
2022-02-26 12:04:02 -0700

    Change from str to uuid

--- cmput_404_project/posts/urls.py
+++ cmput_404_project/posts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.display_public_posts, name='display_public_posts'),
     path('new_post/', views.new_post, name='new_post'),
-    path('own_posts/<slug:id>/', views.edit_post, name='edit_post'),
+    path('own_posts/<uuid:id>/', views.edit_post, name='edit_post'),
     path('own_posts/', views.display_own_posts, name='display_own_posts'),
 ]
