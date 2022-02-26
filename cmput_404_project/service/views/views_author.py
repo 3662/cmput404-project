02f9c9a76a4ce9fb3e2ef64f4fd1ea155d8ac8cb
Jejoon Ryu <rjejoon@gmail.com>
2022-02-26 00:31:43 -0700

    Use baseview class

--- cmput_404_project/service/views/views_author.py
+++ cmput_404_project/service/views/views_author.py
@@ -1,8 +1,9 @@
+import json 
+
 from django.shortcuts import get_object_or_404, render
-from django.views.decorators.http import require_http_methods
 from django.core.paginator import Paginator, EmptyPage
-from django.http import Http404, HttpResponseRedirect, JsonResponse
-from django.db.models import F
+from django.http import Http404, HttpResponseRedirect, JsonResponse, HttpResponse
+from django.views import View
 
 from social_distribution.models import Author
 from accounts.forms import AuthorChangeForm
@@ -11,39 +12,70 @@ DEFAULT_PAGE = 1
 DEFAULT_SIZE = 25
 
 
-@require_http_methods(["GET"])
-def authors_detail(request):
-    '''
-    Returns JSON response of the details of the authors.
 
-    Default page = 1, size = 25
-    '''
-    page = int(request.GET.get('page', DEFAULT_PAGE))
-    size = int(request.GET.get('size', DEFAULT_SIZE))
+class AuthorsDetailView(View):
+    http_method_names = ['get', 'head', 'options']
 
-    try:
-        authors = Paginator(Author.objects.all(), size).page(page)
-    except EmptyPage:
-        raise Http404('Page does not exist')
-    
-    data = {}
-    data['type'] = 'authors'
-    data['items'] = [get_author_detail(author) for author in authors]
-    
-    return JsonResponse(data)
+    def get(self, request, *args, **kwargs):
+        '''
+        Returns JSON response of the details of the authors.
+
+        Default page = 1, size = 25
+        '''
+        return JsonResponse(self._get_authors(request))
+
+    def head(self, request, *args, **kwargs):
+        '''
+        Handles HEAD request the same GET request.
+        '''
+        response = HttpResponse()
+        response.headers['Content-Type'] = 'application/json'
+        response.headers['Content-Length'] = str(len(bytes(json.dumps(self._get_authors(request)), 'utf-8')))
+        return response
+
+    def _get_authors(self, request) -> dict:
+        page = int(request.GET.get('page', DEFAULT_PAGE))
+        size = int(request.GET.get('size', DEFAULT_SIZE))
+
+        try:
+            authors = Paginator(Author.objects.all(), size).page(page)
+        except EmptyPage:
+            raise Http404('Page does not exist')
+
+        data = {}
+        data['type'] = 'authors'
+        data['items'] = [get_author_detail(author) for author in authors]
+
+        return data
 
 
-@require_http_methods(["GET", "POST"])
-def author_detail(request, author_id):
-    '''
-    GET: Returns JSON response of the detail of the author with author_id.
-    POST: Update author_id's profile and returns json response of the author's updated details.
-    '''
-    author = get_object_or_404(Author, pk=author_id)
-    if request.method == 'GET':
+class AuthorDetailView(View):
+    http_method_names = ['get', 'head', 'post', 'options']
+
+    def get(self, request, *args, **kwargs):
+        '''
+        Returns JSON response of the detail of the author with author_id.
+        '''
+        author = get_object_or_404(Author, pk=kwargs.get('author_id', ''))
         return JsonResponse(get_author_detail(author))
-    else:
+
+    def head(self, request, *args, **kwargs):
+        '''
+        Handles HEAD request the same GET request.
+        '''
+        author = get_object_or_404(Author, pk=kwargs.get('author_id', ''))
+        response = HttpResponse()
+        response.headers['Content-Type'] = 'application/json'
+        response.headers['Content-Length'] = str(len(bytes(json.dumps(get_author_detail(author)))), 'utf-8')
+        return response
+
+    def post(self, request, *args, **kwargs):
+        '''
+        Update author_id's profile and returns json response of the author's updated details.
+        '''
         # TODO test POST request
+        author_id = kwargs.pop('author_id', '')
+        author = get_object_or_404(Author, pk=author_id)
         if not request.user.is_authenticated():
             return render(request, '/accounts/login/')     # TODO change redirect url 
         form = AuthorChangeForm(request.POST, instance=request.user)
