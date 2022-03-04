3f489c40c1a73ecb21c55c5d1d50de35af236dea
Jejoon Ryu <rjejoon@gmail.com>
2022-03-04 02:54:55 -0700

    Test cases for FollowerView

--- cmput_404_project/service/tests/tests_follower.py
+++ cmput_404_project/service/tests/tests_follower.py
@@ -3,8 +3,7 @@ from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 
 from social_distribution.models import Author
-from .helper import create_dummy_author_with_followers
-
+from .helper import create_dummy_author_with_followers, create_dummy_authors
 
 
 class FollowersViewTestCase(TestCase):
@@ -63,5 +62,79 @@ class FollowersViewTestCase(TestCase):
             self.assertEqual(data['github'], author.github)
         
         
+class FollowersViewTestCase(TestCase):
 
+    NUM_FOLLOWERS = 1
+
+    def test_get(self):
+        create_dummy_author_with_followers(self.NUM_FOLLOWERS)
+
+        c = Client()
+        author = Author.objects.get(username='test')
+        follower = Author.objects.get(username='test0')
+
+        # test with valid author and follower ids
+        response = c.get(f'/service/authors/{author.id}/followers/{follower.id}/')
+        self.assertEqual(response.status_code, 200)
+
+        # test with valid author id and invalid follower id
+        response = c.get(f'/service/authors/{author.id}/followers/invalid_follower_id/')
+        self.assertEqual(response.status_code, 404)
+
+        # test with invalid author and follower ids
+        response = c.get(f'/service/authors/invalid_author_id/followers/invalid_follower_id/')
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_head(self):
+        create_dummy_author_with_followers(self.NUM_FOLLOWERS)
+
+        c = Client()
+        author = Author.objects.get(username='test')
+        follower = Author.objects.get(username='test0')
+
+        # test with valid author and follower ids
+        response = c.head(f'/service/authors/{author.id}/followers/{follower.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'')
+
+
+    def test_delete(self):
+        create_dummy_author_with_followers(self.NUM_FOLLOWERS)
+
+        c = Client()
+        author = Author.objects.get(username='test')
+        follower = Author.objects.get(username='test0')
+
+        # test with valid author and follower ids
+        response = c.delete(f'/service/authors/{author.id}/followers/{follower.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(not author.followers.filter(id=follower.id).exists())
+
+
+    def test_put(self):
+        create_dummy_authors(2)
+
+        c = Client()
+        author = Author.objects.get(username='test0')
+        foreign_author = Author.objects.get(username='test1')
+
+        # test unauthenticated author
+        response = c.put(f'/service/authors/{author.id}/followers/{foreign_author.id}/')
+        self.assertEqual(response.status_code, 403)
+
+        c.login(username=author.username, password='temporary')
+        # test with valid author and foreign_author ids
+        response = c.put(f'/service/authors/{author.id}/followers/{foreign_author.id}/')
+        self.assertEqual(response.status_code, 201)
+
+        # test with invalid author and foreign_author ids
+        response = c.put(f'/service/authors/invalid_author_id/followers/invalid_foreign_author_id/')
+        self.assertEqual(response.status_code, 404)
+        
+        # test with valid author id and invalid foreign_author id
+        response = c.put(f'/service/authors/{author.id}/followers/invalid_foreign_author_id/')
+        self.assertEqual(response.status_code, 404)
+
+    
 
