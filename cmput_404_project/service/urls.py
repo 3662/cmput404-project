5e9aeea22052d03d2d7ff163ab2caf1307190289
Jejoon Ryu <rjejoon@gmail.com>
2022-03-04 02:55:53 -0700

    Update class name

--- cmput_404_project/service/urls.py
+++ cmput_404_project/service/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('authors/<uuid:author_id>/', views.AuthorDetailView.as_view(), name='author'),
     path('authors/<uuid:author_id>/posts/', views.PostsView.as_view(), name='posts'),
     path('authors/<uuid:author_id>/posts/<uuid:post_id>/', views.PostView.as_view(), name='posts'),
-    path('authors/<uuid:author_id>/followers/', views.FollowersDetailView.as_view(), name='followers_detail'),
+    path('authors/<uuid:author_id>/followers/', views.FollowersView.as_view(), name='followers_detail'),
     path('authors/<uuid:author_id>/followers/<uuid:foreign_author_id>/', views.FollowerView.as_view(), name='follower'),
 ]
--- cmput_404_project/service/views/__init__.py
+++ cmput_404_project/service/views/__init__.py
@@ -1,3 +1,3 @@
 from .views_author import AuthorsDetailView, AuthorDetailView 
-from .views_follower import FollowersDetailView, FollowerView
+from .views_follower import FollowersView, FollowerView
 from .views_post import PostView, PostsView
