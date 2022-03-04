c600ad6b23ee0fa180b9d77c3a6b9b2493ddd471
Jejoon Ryu <rjejoon@gmail.com>
2022-03-04 00:10:45 -0700

    Test case for PostView

--- cmput_404_project/service/tests/tests_post.py
+++ cmput_404_project/service/tests/tests_post.py
@@ -1,9 +1,12 @@
-from tkinter import W
-from venv import create
+import uuid
+import json
+
 from django.test import TestCase, Client
+from django.core.exceptions import ObjectDoesNotExist
 
 from social_distribution.models import Author, Post
 from .helper import create_dummy_authors, create_dummy_post
+
 
 
 class PostViewTestCase(TestCase):
@@ -55,7 +58,121 @@ class PostViewTestCase(TestCase):
         author = Author.objects.get(username='test0')
         visibility = 'PUBLIC'
         create_dummy_post(author, visibility=visibility, content_type='text/plain')
-        # self.assertEqual(response.status_code, 403)
+        post = Post.objects.get(title='Test Post')
+
+        # test without being signed in
+        response = c.post(f'/service/authors/{author.id}/posts/{post.id}/')
+        self.assertEqual(response.status_code, 403)
+
+        c.login(username=author.username, password='temporary')
+
+        before_published = post.published
+        before_modified = post.modified
+
+        # post with valid data
+        data = {
+            'title': 'Updated Test Post Title',
+            'description': 'Updated Test Post description',
+            'content_type': 'text/plain',
+            'content': 'Updated test post content', 
+            'categories': 'updated,test,post,categories',
+            'visibility': 'PUBLIC',
+        }
+        response = c.post(f'/service/authors/{author.id}/posts/{post.id}/', data)
+        self.assertEqual(response.status_code, 200)
+
+        post = Post.objects.get(id=post.id)     # get updated post
+
+        # test timestamps
+        self.assertTrue(before_published == post.published)
+        self.assertTrue(before_modified < post.modified)
+
+        # test updated fields 
+        response = c.get(f'/service/authors/{author.id}/posts/{post.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assert_post_data(post, author, response.json())
+
+        # post with invalid data
+        data = {
+            'title': 'Updated Test Post Title',
+            'description': 'Updated Test Post description',
+            'content_type': 'text/plain',
+            'content': 'Updated test post content', 
+            # missing data
+        }
+        response = c.post(f'/service/authors/{author.id}/posts/{post.id}/', data)
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_delete(self):
+        c = Client()
+
+        author = Author.objects.get(username='test0')
+        visibility = 'PUBLIC'
+        create_dummy_post(author, visibility=visibility, content_type='text/plain')
+        post = Post.objects.get(title='Test Post')
+
+        response = c.delete(f'/service/authors/{author.id}/posts/{post.id}/')
+        self.assertEqual(response.status_code, 200)
+
+        # make sure the post is deleted from database
+        with self.assertRaises(ObjectDoesNotExist):
+            Post.objects.get(id=post.id)
+
+        response = c.delete(f'/service/authors/{author.id}/posts/{post.id}/')
+        self.assertEqual(response.status_code, 404, 'Retrieving deleted post should return 404')
+
+
+    def test_put(self):
+        c = Client()
+        author = Author.objects.get(username='test0')
+        post_id = uuid.uuid4()
+        data = {
+            'title': 'Test Post',
+            'description': 'Test Post description',
+            'content_type': 'text/plain',
+            'content': 'Test post content', 
+            'categories': 'test,post,categories',
+            'visibility': 'PUBLIC',
+        }
+        response = c.put(f'/service/authors/{author.id}/posts/{post_id}/', json.dumps(data))
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(Post.objects.filter(id=post_id, author=author).exists())
+
+        # test whether the data is saved in db
+        response = c.get(f'/service/authors/{author.id}/posts/{post_id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assert_post_data(Post.objects.get(id=post_id, author=author), author, response.json())
+
+        data = {
+            'title': 'Updated Test Post',
+            'description': 'Updated Test Post description',
+            'content_type': 'text/plain',
+            'content': 'Updated Test post content', 
+            'categories': 'test,post,categories',
+            'visibility': 'PUBLIC',
+        }
+        # test with non-json data
+        response = c.put(f'/service/authors/{author.id}/posts/{post_id}/', data)
+        self.assertEqual(response.status_code, 400)
+        # test update without being authenticated
+        response = c.put(f'/service/authors/{author.id}/posts/{post_id}/', json.dumps(data))
+        self.assertEqual(response.status_code, 403)
+        # test with valid json data and authenticated user
+        c.login(username=author.username, password='temporary')
+        response = c.put(f'/service/authors/{author.id}/posts/{post_id}/', json.dumps(data))
+        self.assertEqual(response.status_code, 200)
+        # test with invalid data
+        data.pop('title')
+        response = c.put(f'/service/authors/{author.id}/posts/{post_id}/', data)
+        self.assertEqual(response.status_code, 400)
+
+        # test whether the data is saved in db
+        response = c.get(f'/service/authors/{author.id}/posts/{post_id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assert_post_data(Post.objects.get(id=post_id, author=author), author, response.json())
+
+
 
     def assert_post_data(self, post, author, data):
         # TODO test source, origin, comments
