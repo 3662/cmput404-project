0b2f0af69308b391813a9456a928083472209a6a
Jejoon Ryu <rjejoon@gmail.com>
2022-03-03 17:11:09 -0700

    Write test cases for AuthorDetailView

--- cmput_404_project/service/tests/tests_author.py
+++ cmput_404_project/service/tests/tests_author.py
@@ -1,9 +1,10 @@
-import json
-
 from django.test import TestCase, Client
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 from social_distribution.models import Author
 from .helper import create_dummy_authors
+
 
 class AuthorsDetailViewTestCase(TestCase):
 
@@ -43,7 +44,88 @@ class AuthorsDetailViewTestCase(TestCase):
         self.assertEqual(response.content, b'')
 
 
+class AuthorDetailViewTestCase(TestCase):
 
+    def setUp(self):
+        create_dummy_authors(1)
+
+    def test_get(self):
+        c = Client()
+
+        author = Author.objects.get(username='test0')
+        response = c.get(f'/service/authors/{author.id}/')
+
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTrue('type' in data.keys())
+        self.assertTrue('id' in data.keys())
+        self.assertTrue('url' in data.keys())
+        self.assertTrue('host' in data.keys())
+        self.assertTrue('displayName' in data.keys())
+        self.assertTrue('github' in data.keys())
+        self.assertTrue('profileImage' in data.keys())
+
+        self.assertEqual(data['type'], 'author')
+        self.assertEqual(data['displayName'], author.get_full_name())
+        self.assertEqual(data['profileImage'], author.profile_image)
+        try:
+            validate = URLValidator()
+            validate(data['id'])
+            validate(data['url'])
+            validate(data['github'])
+        except ValidationError as e:
+            self.assertTrue(False, "This field must be a valid url")
+        else:
+            self.assertEqual(data['id'], f'{author.host}authors/{author.id}')
+            self.assertEqual(data['url'], f'{author.host}authors/{author.id}')
+            self.assertEqual(data['github'], author.github)
+
+    def test_head(self):
+        c = Client()
+
+        author = Author.objects.get(username='test0')
+        response = c.head(f'/service/authors/{author.id}/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'')
+
+    
+    def test_post(self):
+        c = Client()
+
+        author = Author.objects.get(username='test0')
+
+        # test
+        response = c.post(f'/service/authors/{author.id}/')
+        self.assertEqual(response.status_code, 403)
+
+        c.login(username='test0', password='temporary')
+
+        # post with invalid form
+        data = {
+            'github': 'invalid url',
+        }
+        response = c.post(f'/service/authors/{author.id}/', data, follow=True)
+        self.assertEqual(response.status_code, 400)
+
+        # post with valid form
+        data = {
+            'first_name': 'Updated_first_name',
+            'last_name': 'Updated_last_name',
+            'profile_image': 'https://avatars.githubusercontent.com/u/71972141?s=200&v=4',
+            'github': 'https://github.com/updated',
+        }
+        response = c.post(f'/service/authors/{author.id}/', data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        # check if the author is updated
+        author = Author.objects.get(username='test0')
+        self.assertEqual(author.first_name, data['first_name'])
+        self.assertEqual(author.last_name, data['last_name'])
+        self.assertEqual(author.profile_image, data['profile_image'])
+        self.assertEqual(author.github, data['github'])
+        
 
     
 
