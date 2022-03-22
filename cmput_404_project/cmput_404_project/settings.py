489960c491edfa518ed61f9108acb9363e0a53a7
Jejoon Ryu <rjejoon@gmail.com>
2022-03-22 15:31:32 -0600

    Update STATIC vars

--- cmput_404_project/cmput_404_project/settings.py
+++ cmput_404_project/cmput_404_project/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = (
+    STATIC_ROOT,
+)
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
