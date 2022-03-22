0d5f0df339e9663cdb4d7db43443e11ca1aa8e0a
Jejoon Ryu <rjejoon@gmail.com>
2022-03-22 15:21:37 -0600

    Update STATIC_ROOT for deployment

--- cmput_404_project/cmput_404_project/settings.py
+++ cmput_404_project/cmput_404_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.path.join(BASE_DIR, "static/")
 STATIC_ROOT = '/social-distribution/static/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
