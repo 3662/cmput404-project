b56600c9f146b4249e507eb059ec4da36d195107
Jejoon Ryu <rjejoon@gmail.com>
2022-03-22 15:18:07 -0600

    Set STATIC_ROOT for deployment

--- cmput_404_project/cmput_404_project/settings.py
+++ cmput_404_project/cmput_404_project/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
