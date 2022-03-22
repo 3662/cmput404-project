8bd0233d8c781b38a5dc24004a9d82347a7c516c
Jejoon Ryu <rjejoon@gmail.com>
2022-03-22 15:32:32 -0600

    Update STATIC_ROOT for deployment

--- cmput_404_project/cmput_404_project/settings.py
+++ cmput_404_project/cmput_404_project/settings.py
@@ -129,8 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = (
-    STATIC_ROOT,
-)
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
