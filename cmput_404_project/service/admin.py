c6e45bed2c5dac002e29c1522b50179948d8fef5
Jejoon Ryu <rjejoon@gmail.com>
2022-03-22 22:19:09 -0600

    Hash the password

--- cmput_404_project/service/admin.py
+++ cmput_404_project/service/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from service.models import ServerNode
 
 
--- cmput_404_project/service/models.py
+++ cmput_404_project/service/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.dispatch import receiver 
+from django.db.models.signals import post_save 
+from django.contrib.auth.hashers import make_password
+
+from service.managers import ServerNodeManager
 
 
 class ServerNode(models.Model):
@@ -10,4 +15,16 @@ class ServerNode(models.Model):
     username = models.CharField(max_length=100, null=False)
     password = models.CharField(max_length=128, null=False)
 
+    def __str__(self):
+        return self.host
     
+
+
+@receiver(post_save, sender=ServerNode)
+def create_inbox(sender, instance, created, **kwargs):
+    '''
+    Upon ServerNode creation, hash the plain password and save it. 
+    '''
+    if created:
+        instance.password = make_password(instance.password)
+        instance.save()
