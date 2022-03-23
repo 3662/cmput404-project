26aa394d338f1e3a1c057705c578e931286133cd
Jejoon Ryu <rjejoon@gmail.com>
2022-03-22 22:19:31 -0600

    Remove unused imports

--- cmput_404_project/service/models.py
+++ cmput_404_project/service/models.py
@@ -3,8 +3,6 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save 
 from django.contrib.auth.hashers import make_password
 
-from service.managers import ServerNodeManager
-
 
 class ServerNode(models.Model):
 
