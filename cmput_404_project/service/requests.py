62593305f57c34355bbf6e0b59e5e390e57c14b0
Jejoon Ryu <rjejoon@gmail.com>
2022-04-04 01:14:55 -0600

    Allow to put scheme in ServerNode host field

--- cmput_404_project/service/requests.py
+++ cmput_404_project/service/requests.py
@@ -13,8 +13,8 @@ def get_b64_server_credential(server_host: str):
     Note: You must set up ServerNode in admin page if you want to make a request to a remote server.
 
     Example server_host: 
-        - 127.0.0.1:5000
-        - cmput-404-w22-project-group09.herokuapp.com
+        - http://127.0.0.1:5000
+        - https://cmput-404-w22-project-group09.herokuapp.com
 
     Example Usage:
 
